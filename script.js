fe01517e15ba0c48616c21d2e685ccd0a240d0a0
MaRos10 <148355147+MaRos10@users.noreply.github.com>
2024-04-08 09:57:11 +0200

    Added automatic update to copyright-year

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+/* Menu */
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+/* Automatically updating footer copyright year */
+const year = document.querySelector("#current-year");
+year.innerHTML = new Date().getFullYear();
