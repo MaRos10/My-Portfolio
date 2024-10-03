c74cec6461a8231a38eff55ff0761566b51d9586
MaRos10 <148355147+MaRos10@users.noreply.github.com>
2024-10-03 08:51:39 +0200

    Added description with animation to project-img

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function toggleMenu() {
 /* Automatically updating footer copyright year */
 const year = document.querySelector("#current-year");
 year.innerHTML = new Date().getFullYear();
+
+/* Flipping project-img to see more information */
+document.querySelectorAll(".project-card").forEach((card) => {
+  card.addEventListener("click", function () {
+    card.classList.toggle("flipped");
+  });
+});
